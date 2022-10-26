53a0649051a48977c66ed693ad177bacf7bdf25b
TejaswiniK26 <tejaswinikattukuri@gmail.com>
2022-10-26 16:50:39 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res, next) {
-    var n1 = Math.floor(Math.random() * 10);
-    var n2 = Math.floor(Math.random() * 10);
-    var n3 = Math.floor(Math.random() * 10);
-    var atanFunction = Math.atan(n1);
-    var expFunction = Math.exp(n2);
-    var expm1Function = Math.expm1(n3);
+    var number1 = Math.floor(Math.random() * 10);
+    var number2 = Math.floor(Math.random() * 10);
+    var number3 = Math.floor(Math.random() * 10);
+    var atanFunction = Math.atan(number1);
+    var expFunction = Math.exp(number2);
+    var expm1Function = Math.expm1(number3);
     res.render('computation',
                 {
                     title: 'Tejaswini Kattukuri computing functions',
-                    n1: n1,
-                    n2: n2,
-                    n3: n3,
+                    number1: number1,
+                    number2: number2,
+                    number3: number3,
                     atan: atanFunction,
                     exp: expFunction,
                     expm1: expm1Function
